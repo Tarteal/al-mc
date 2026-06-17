7332a8ec6a1fe30fc0d6283195ef58d687e4625b
Tarteal <tarteal2021@gmail.com>
2026-06-17 17:32:11 +0500

    Fix: Add TanStack Start and Router plugins to Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { tanstackStartVite } from "@tanstack/react-start/vite";
 
 export default defineConfig({
   plugins: [
-    // plugins here
+    tanstackStartVite(),
+    TanStackRouterVite(),
+    react(),
   ],
 });
